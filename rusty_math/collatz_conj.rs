d5d0235a0e651cbd5e88074ab559f55b674c905d
Dylan Corbett <dylancorbett@icloud.com>
2018-06-20 23:09:15 -0700

    Add collatz to min fn

--- rusty_math/collatz_conj.rs
+++ rusty_math/collatz_conj.rs
@@ -1,7 +1,5 @@
 use std::os;
 
-mod collatz;
-
 fn main() {
     if os::args().len() < 2 {
         println!("Error: please provide a number as argument.");
@@ -11,4 +9,13 @@ fn main() {
 
     let i = from_str::<int>(os::args()[1].unwrap();
     println!("{:d} has {:d} Collatz steps", i, collatz(i));
+}
+
+
+fn collatz(N: int) -> int {
+    if N == 1 { return 0; }
+    match N % 2 {
+        0 => { 1 + collatz(N/2) }
+        _ => { 1 + collatz(N*3+1) }
+    }
 }
